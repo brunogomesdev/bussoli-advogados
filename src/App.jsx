6eb688d4dbe06d84e1960dae539276299b052cf7
brunogomesdev <brunowd@gmail.com>
2024-09-14 19:33:07 -0300

    ajustes na home

--- src/App.jsx
+++ src/App.jsx
@@ -32,30 +32,33 @@ function App() {
       </div>
 
       <div className="main-atuacao">
+        <div className="atuacao-titulo">
+          Áreas de Atuação
+        </div>
         <div className="atuacao">
           <div className="atuacao-list">
             <p>Atuação 01</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
           </div>
           <div className="atuacao-list">
             <p>Atuação 02</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
           </div>
           <div className="atuacao-list">
             <p>Atuação 03</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
           </div>
           <div className="atuacao-list">
             <p>Atuação 04</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
           </div>
           <div className="atuacao-list">
             <p>Atuação 05</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
           </div>
           <div className="atuacao-list">
             <p>Atuação 06</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
           </div>
         </div>
       </div>
@@ -63,14 +66,27 @@ function App() {
       <div className="main-parceiros">
         <div className="parceiros">
           <p className="tit-parceiros">Parceiros</p>
-          <p className="txt-parceiros">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
+          <p className="txt-parceiros">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
       </div>
 
       <div className="main-advogados">
+        <div className="advogados-titulo">
+          Advogados
+        </div>
         <div className="advogados">
-          <p className="tit-advogados">Advogados</p>
-          <p className="txt-advogados">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
+          <div className="advogados-list">
+            <p>Advogado 01</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+          </div>
+          <div className="advogados-list">
+            <p>Advogado 02</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+          </div>
+          <div className="advogados-list">
+            <p>Advogado 03</p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+          </div>
         </div>
       </div>
 
@@ -78,6 +94,22 @@ function App() {
         <div className="contato">
           <p className="tit-contato">Contato</p>
           <p className="txt-contato">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
+        </div>
+      </div>
+
+      <div className="main-rodape">
+        <div className="rodape">
+          <div className="logo">
+            <a href="https://vitejs.dev" target="_blank">
+              <img src={logoBussoli} className="logo" alt="Vite logo" />
+            </a>
+          </div>
+          <div className="menu">
+            <div className="menu-itens">ATUAÇÃO</div>
+            <div className="menu-itens">PARCEIROS</div>
+            <div className="menu-itens">ADVOGADOS</div>
+            <div className="menu-itens">CONTATO</div>
+          </div>
         </div>
       </div>
 
